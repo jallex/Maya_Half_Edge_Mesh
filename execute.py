d074d3efbe2805a119b1c74a783150538db4c2fa
Jackie Allex <44556715+jallex@users.noreply.github.com>
2021-12-23 18:08:29 -0500

    Update execute.py

--- execute.py
+++ execute.py
@@ -4,16 +4,16 @@ import sys
 
 # SET THIS FOLDER variable to the path of the parent folder that you've downloaded the repository to
 # or add to Maya's scripts folder or ensure that the parent folder is added to your PYTHONPATH
-folder = '/Maya_Half_Edge_Mesh/'
+folder = '/Maya_LOD_Half_Edge_Mesh/'
 
 #check if folder is part of PYTHONPATH and if not, add it
 if folder not in sys.path:
     sys.path.append(folder)
 
-if 'Maya_Half_Edge_Mesh' in sys.modules:
-    del sys.modules['Maya_Half_Edge_Mesh']
-if 'Maya_Half_Edge_Mesh.half_edge' in sys.modules:
-    del sys.modules['Maya.half_edge']
-import Maya_Half_Edge_Mesh.half_edge
+if 'Maya_LOD_Half_Edge_Mesh' in sys.modules:
+    del sys.modules['Maya_LOD_Half_Edge_Mesh']
+if 'Maya_LOD_Half_Edge_Mesh.half_edge' in sys.modules:
+    del sys.modules['Maya_LOD_Half_Edge_Mesh.half_edge']
+import Maya_LOD_Half_Edge_Mesh.half_edge
 
-window = Maya_Half_Edge_Mesh.half_edge.showWindow()
+window = Maya_LOD_Half_Edge_Mesh.half_edge.showWindow()
